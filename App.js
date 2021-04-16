98bff75428cae7cb18bd6869ccb1418f0e9b5fef
Sam Chillcott <samchillcott@gmail.com>
2021-04-16 09:47:26 +0100

    create list components

--- App.js
+++ App.js
@@ -1,12 +1,24 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, View } from 'react-native';
+import React, { useState } from 'react';
+import { FlatList, StyleSheet, View, Text } from 'react-native';
 import Header from './components/Header';
+import ListItem from './components/ListItem'
 
 export default function App() {
+  const [items, setItems] = useState([
+    {id: 1, text: "milk"},
+    {id: 2, text: "eggs"},
+    {id: 3, text: "bread"},
+    {id: 4, text: "juice"}
+  ]);
+
   return (
     <View style={styles.container}>
       <Header title="Shopping List"/>
+      <FlatList
+        data={items}
+        renderItem={({item}) => <ListItem item={item}></ListItem>}
+        />
       <StatusBar style="auto" />
     </View>
   );
---
+++ components/ListItem.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import { StyleSheet, Text, View } from 'react-native'
+
+const ListItem = () => {
+    return (
+        <View>
+            <Text>hi</Text>
+        </View>
+    )
+}
+
+export default ListItem
+
+const styles = StyleSheet.create({})
