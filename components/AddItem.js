9eda93ef8a79333e7c9895bf7b0391df0d8d85f3
Sam Chillcott <samchillcott@gmail.com>
2021-04-16 15:43:52 +0100

    add handler/state

--- components/AddItem.js
+++ components/AddItem.js
@@ -1,23 +1,29 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { StyleSheet, Text, View, TextInput, TouchableOpacity } from 'react-native'
 import { Ionicons } from '@expo/vector-icons';
 
 const AddItem = () => {
+    const [text, setText] = useState('');
+
+    const onChange = (textValue) => setText(textValue);
+
     return (
         <View>
             <TextInput
                 placeholder="Add Item"
                 style={styles.input}
+                onChangeText={onChange}
+                value={text}
             />
             <TouchableOpacity style={styles.btn}>
                 <Text
                     style={styles.btnText}>
-                    Add Item
                     <Ionicons
                     name="md-add"
                     size={32}
                     // onPress={() => deleteItem(item.id)}
-                />
+                    />
+                    Add Item
                 </Text>
 
             </TouchableOpacity>
@@ -26,7 +32,21 @@ const AddItem = () => {
 }
 
 const styles = StyleSheet.create({
-
+    input:{
+        height: 60,
+        padding: 8,
+        margin: 5,
+    },
+    btn: {
+        backgroundColor: '#c2bad8',
+        padding: 9,
+        margin: 8,
+    },
+    btnText: {
+        color: 'darkslateblue',
+        fontSize: 20,
+        textAlign: 'center',
+    }
 })
 
 export default AddItem;
